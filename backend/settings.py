e512ffe58cbc12fb0a572d03858a48d2ed4cd1b0
JAYASIMA007 <jayasima.k.cse.2021@snsct.org>
2025-07-11 13:14:13 +0530

    Add .env file for environment variables and update CORS allowed origins

--- backend/settings.py
+++ backend/settings.py
@@ -63,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://leaderboard-frontend-vert.vercel.app"
+    "https://snsct-leaderboard.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
